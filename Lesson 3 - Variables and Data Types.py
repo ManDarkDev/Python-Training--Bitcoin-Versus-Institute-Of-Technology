952a3fdac4540e73e9061d52a5ea075d279013e6
ManDarkDev <matthanburton@gmail.com>
2024-07-12 07:24:44 -0500

    Add files via upload

--- Lesson 3 - Variables and Data Types.py
+++ Lesson 3 - Variables and Data Types.py
@@ -1,16 +1,31 @@
+#You can say what you want about Michael Jordan. You can Say what you'd like about Kobe. Kareem Abdul-Jabar is also a vaild choice.
+#But Lebron is the most computationally efficient on the basketball court. He's also the most physically gifted.
+#Michael Jordan may have been Beehtoven. But Lebron was - and still is - the young and naturally talented Mozart. 
+#Kobe. Ah, yes, but of course. The son of a pro. The Black Mamba. The "Prince of all Saiyans." I'd dare to say he was just as skilled as Michael.
+#Statistically speaking, Kobe's overall body of work (Less Titles, Less MVPs, Less Scoring titles, and most missed shots ever), would suggest that he's 
+#A close second to Jordan. And so that leaves us to the now age-old debate: 
+#Is Lebron Better than Michael Jordan?
+
 character_name = "Lebron" 
+sons_name = "Bronny James"
 character_stats = "27 Points per game"
-character_age = "40"
+old_Lebron = "40"
+young_Lebron = "18"
+team_mate = True #Booleans are cool but may not really need as much as other variables and Data Types
+
 print(character_name + " is better than jordan")
 print("Ultimately," + character_name + "has more points, rebounds, and assist than Jordan")
 print("Altthough lebron averaged less points per game at " + character_stats)
 print("He was an overall better teammate than Jordan")
 print("We've never seen anyone as old and athletic as " + character_name )
 print('#'* 70)
-print("Lebron will turn " + character_age + " this year")
-print("And everyone knows he's the greatest " + character_age + " year old")
+print("Lebron will turn " + old_Lebron + " this year")
+print("And everyone knows he's the greatest " + young_Lebron + " year old")
 print("That the world has ever seen play in the NBA")
 print('#'* 70)
-character_name = "Bronny"
+
 print("With " + character_name + " being drafted in the 2nd round")
-print("This will also be the first time " + "and son have played together")
+
+print("This will also be the first time " + character_name + " and " + sons_name + " have played together") #make sure you put the addition sign before and after
+
+print(team_mate)
