d858fac4fa2252d07008398b14407b06b20d8fd8
ManDarkDev <matthanburton@gmail.com>
2024-07-12 07:23:49 -0500

    Add files via upload

---
+++ Lesson 5 - Working with Numbers.py
@@ -0,0 +1,40 @@
+'''I hate arithmetic. It's never been my thing. I was always someone who learned from a historical perspective. What happened? How did pythagorus find his theorem? 
+Isaac Newtons Gravitational Equations are cool, from a philosophical perspective, but I don't care for the equations. The equations just verify the philosophy. 
+The eqauations confirm the thoughts, but it never preceeds the imagination. Stay away from stupid people. Keep your imagination sharp. Dare to dream. '''
+from math import * #gathers new math functions that you can use in python
+
+print(2 + 2)
+print(100)
+print(3.5)
+print(230 - 75)
+
+print(10*100)
+print(5*7+25)
+print(20%6)#the modulus operator gives you the remainder not the value
+
+print(9**9) #prints the exponential value 9 to the 9th power
+
+num_ber = 100
+print(num_ber + 100)
+
+print("I keep it " + str(num_ber))#put numbers inside of strings with the "str" command
+
+my_num = -10
+print(abs(my_num)) #prints the absolute value of 10
+
+print(pow(3 ,6)) # 3^6 or 3 to the power of 6
+
+print(max(95000, 25)) #prints largest numnber
+
+print(min(1000, 5000))#prints smallest number 
+
+print(round(7.25)) #rounds number to nearest whole number
+
+print(floor(1.9))#floor rounds down
+
+print(ceil(77.1))#ceil rounds up to nearest whole number
+
+print(sqrt(50)) #sqrt return the square root of a number
+
+
+
