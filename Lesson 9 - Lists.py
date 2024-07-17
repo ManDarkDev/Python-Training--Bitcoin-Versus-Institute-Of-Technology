0d824e6c441d8a215abef986bb76fc893f41de90
ManDarkDev <matthanburton@gmail.com>
2024-07-17 08:54:24 -0500

    Add files via upload

---
+++ Lesson 9 - Lists.py
@@ -0,0 +1,10 @@
+friends = ["Eric", "Nazzir", "Jason", "Tanner"]
+           # 0         1          2       3
+           #index starts count with 0
+print(friends)
+
+print(friends[1])
+
+print(friends[-1]) #starts count at the back of the list
+
+print(friends[0:3])
