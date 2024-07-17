0611a68463610cd3938429d4fdd8b1815d3bcac3
ManDarkDev <matthanburton@gmail.com>
2024-07-17 08:23:37 -0500

    Update Lesson 7 - Calculator.py

--- Lesson 7 - Calculator.py
+++ Lesson 7 - Calculator.py
@@ -1,6 +1,6 @@
 num1 = input("Enter a number: ")
 num2 = input("Enter a 2nd number: ")
-result = int(num1) + float(num2) #converts the string to a number
+result = int(num1) + int(num2) #converts the string to a number
 
 print(result)
 
