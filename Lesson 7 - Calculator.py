1b6d86457d098192a10bfe7f4d0643cbcaa39cba
ManDarkDev <matthanburton@gmail.com>
2024-07-17 08:22:25 -0500

    Add files via upload

---
+++ Lesson 7 - Calculator.py
@@ -0,0 +1,7 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter a 2nd number: ")
+result = int(num1) + float(num2) #converts the string to a number
+
+print(result)
+
+
---
+++ Lesson 8 - Mad Libs.py
@@ -0,0 +1,13 @@
+
+
+
+country = input("Which country: ")
+celebrity = input ("Name your favorite rapper: ")
+food = input ("What kind of food do you like: ")
+
+
+
+
+print("If I could visit one country, it would be " + country)
+print("If I could smoke with one rapper, dead or alive, it would probably be " + celebrity)
+print("When I go out to eat, I usually order " + food )
