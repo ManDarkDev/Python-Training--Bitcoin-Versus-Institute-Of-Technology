ba32f4fb41978da434fdc59135fbe72abf66cdca
ManDarkDev <matthanburton@gmail.com>
2024-07-24 12:00:34 -0500

    Add files via upload

--- Lesson 16 - Building a Better Calculator.py
+++ Lesson 16 - Building a Better Calculator.py
@@ -1,12 +1,14 @@
-num1 = float(input ("Enter 1st number: "))
-op = input("Enter Operator: ")
-num2 = float(input("Enter second number: "))
+#Great lesson. Building calculators is a great way to get started with learning about input and comparisons. 
 
-if op == "+":
-    print(num1 + num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
+num1 = float(input ("Enter 1st number: "))  # Prompt the user to enter the first number, convert it to float, and store it in 'num1'
+op = input("Enter Operator: ") # Prompt the user to enter an operator and store it in 'op'
+num2 = float(input("Enter second number: "))  # Prompt the user to enter the second number, convert it to float, and store it in 'num2'
+
+if op == "+": #if user puts in + sign 
+    print(num1 + num2) #then the two numbers will be added
+elif op == "/": #if user puts in / sign 
+    print(num1 / num2)#then the two numbers will be divided
+elif op == "*": #if user puts in * sign 
+    print(num1 * num2)#then the two numbers will be multiplied
 else:
-    print("Invalid Operator!")
+    print("Invalid Operator!") #any other operator input besides +, -, or * is invalid
