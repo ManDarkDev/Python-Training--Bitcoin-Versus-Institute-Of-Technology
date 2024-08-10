493d132909287b26355957c25836a0eaa05217e3
ManDarkDev <matthanburton@gmail.com>
2024-08-10 10:57:34 -0500

    Update Lesson 17 - Dictionaries.py

--- Lesson 17 - Dictionaries.py
+++ Lesson 17 - Dictionaries.py
@@ -10,8 +10,9 @@ BitcoinTerminology = {
            "TH/s": "Terrahash per second",
            "IP": "IP Address",
            "SN": "Serial Number",
-           "MAC": "Media Access Control Address", 
-           "IT": "Information Technology"
+           "MAC": "Media Access Control Address - a unique identifier assigned to network interfaces for communication on the physical network segment. It is represented in hexadecimal format, typically as six pairs of hexadecimal digits separated by colons or hyphens (e.g., 00:1A:2B:3C:4D:5E or 00-1A-2B-3C-4D-5E). Each pair represents 8 bits, so a MAC address is 48 bits (6 bytes) long.",
+           "IT": "Information Technology -  a set of related fields that encompass computer systems, software, programming languages, and data and information processing, and storage",
+           "API":"Application Programming Interface - a set of rules, protocols, and tools that enables software applications to communicate with each other, exchange data, and share functionality. It acts as an intermediary between different software systems, allowing them to interact and integrate seamlessly.",
            }
 
 print(BitcoinTerminology["CB"]) #Prints out 'Control Board')
